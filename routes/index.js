bbca56b53e83db54cbb4bcf3307208a59fabcfbd
hassanbaryal <h.baryal2@gmail.com>
2023-02-11 16:56:28 -0500

    Added routes to login and signup pages

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,16 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
+  res.redirect('/login');
+});
+
+// GET login page
+router.get('/login', (req, res) => {
+  res.render('login', { title: 'Members Only' });
+});
+
+router.get('/signup', (req, res) => {
+  res.render('signup', { title: 'Members Only' });
 });
 
 module.exports = router;
