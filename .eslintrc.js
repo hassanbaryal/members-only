9f07ca8a34c1967a6d103d54041f15358288a372
hassanbaryal <h.baryal2@gmail.com>
2023-02-11 12:14:19 -0500

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier', 'plugin:n/recommended'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-underscore-dangle': 'off',
+    'func-names': 'off',
+    'no-console': 'off',
+    'camelcase': 'off',
+  },
+};
