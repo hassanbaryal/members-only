2415f6f290b709ee263725b8d4ec032771d2d799
hassanbaryal <h.baryal2@gmail.com>
2023-02-11 20:29:22 -0500

    Created mode for user

---
+++ models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  member: { type: Boolean, required: true, default: false },
+});
+
+userSchema.virtual('url').get(function () {
+  return `/profile/${this.userSchema}`;
+});
+
+module.exports = mongoose.model('User', userSchema);
