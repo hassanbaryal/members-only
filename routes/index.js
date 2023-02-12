df8f94650a6abf8261814cb0e235ee6ffe004b6e
hassanbaryal <h.baryal2@gmail.com>
2023-02-12 18:32:35 -0500

    Added routes for login, logout, and homepage

--- routes/index.js
+++ routes/index.js
@@ -11,10 +11,19 @@ router.get('/', (req, res) => {
 // GET login page
 router.get('/login', userController.login_get);
 
+// POST login
+router.post('/login', userController.login_post);
+
 // GET sign up page
 router.get('/signup', userController.signup_get);
 
 // POST sign up page
 router.post('/signup', userController.signup_post);
+
+// GET logout
+router.get('/logout', userController.logout_get);
+
+// GET homepages
+router.get('/homepage', userController.homepage_get);
 
 module.exports = router;
