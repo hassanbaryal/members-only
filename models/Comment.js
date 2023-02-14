daa032b623d643ff8fda33211acd2a2d981684c6
hassanbaryal <h.baryal2@gmail.com>
2023-02-13 20:05:56 -0500

    Created models for Posts and Comments

---
+++ models/Comment.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
+
+const { Schema } = mongoose;
+
+const commentSchema = new Schema({
+  text: { type: String, required: true, minLength: 1, maxLength: 1000 },
+  timeStamp: { type: Date, default: Date.now },
+  user: { type: Schema.Types.ObjectId, ref: 'User' },
+  post: { type: Schema.Types.ObjectId, ref: 'Post' },
+});
+
+commentSchema.virtual('formattedTimeStamp').get(function () {
+  return DateTime.fromJSDate(this.timeStamp).toLocaleString(
+    DateTime.DATETIME_MED
+  );
+});
+
+module.exports = mongoose.model('Comment'.commentSchema);
---
+++ models/Post.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
+
+const { Schema } = mongoose;
+
+const postSchema = new Schema({
+  title: { type: String, required: true, minLength: 1, maxLength: 100 },
+  text: { type: String, required: true, minLength: 1, maxLength: 1000 },
+  timeStamp: { type: Date, default: Date.now },
+  user: { type: Schema.Types.ObjectId, ref: 'User' },
+  likes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+});
+
+postSchema.virtual('url').get(function () {
+  return `post/${this._id}`;
+});
+
+postSchema.virtual('formattedTimeStamp').get(function () {
+  return DateTime.fromJSDate(this.timeStamp).toLocaleString(
+    DateTime.DATETIME_MED
+  );
+});
+
+module.exports = mongoose.model('Post', postSchema);
