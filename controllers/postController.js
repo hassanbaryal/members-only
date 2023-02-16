e8495bbc54ac68a5a9cdb60d7891c85fb257f939
hassanbaryal <h.baryal2@gmail.com>
2023-02-16 15:46:42 -0500

    Changed redirect URL

--- controllers/postController.js
+++ controllers/postController.js
@@ -56,10 +56,9 @@ exports.createPost_post = [
 
 // Delete post on POST
 exports.deletePost_post = (req, res, next) => {
-  console.log(req.params.id, req.url);
   Post.deleteOne({ _id: req.params.id }).exec((err) => {
     if (err) return next(err);
-    return res.redirect('back');
+    return res.redirect('/profile/');
   });
 };
 
